5919501e6211f3acd3d8d5d33bcc96d42aa16950
Clembs <clembs@clembs.xyz>
2022-02-22 13:24:23 +0100

    Initial commit

---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let title: string = undefined;
+	export let description: string = undefined;
+
+	let tags = {
+		site_name: 'VerticalVision',
+		title: title ? `VerticalVision - ${title}` : 'VerticalVision - Watch your safety',
+		description,
+		baseUrl: $page.url.host,
+		image: '/logo.png',
+		color: '#3A7DC9'
+	};
+</script>
+
+<svelte:head>
+	<title>{tags.title}</title>
+	<meta name="author" content={tags.site_name} />
+	<meta name="title" content={tags.title} />
+	{#if description} <meta name="description" content={tags.description} /> {/if}
+	<meta name="copyright" content="&copy;2022-{new Date().getFullYear()} Vincent Voisin" />
+	<meta name="theme-color" content={tags.color} />
+	<!-- Google index -->
+	<meta name="”robots”" content="”follow,index”" />
+	<meta name="”googlebot”" content="”follow,index”" />
+	<link rel="canonical" href={tags.baseUrl} />
+	<!-- OpenGraph -->
+	<meta property="og:site_name" content={tags.site_name} />
+	<meta property="og:title" content={tags.title} />
+	{#if description} <meta property="og:description" content={tags.description} /> {/if}
+	<meta property="og:url" content="{tags.baseUrl}{$page.url.pathname}" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content={tags.image} />
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_small_image" />
+	<meta name="twitter:site" content={tags.site_name} />
+	<meta name="twitter:title" content={tags.title} />
+	{#if description}<meta name="twitter:description" content={tags.description} /> {/if}
+	<meta name="twitter:image" content={tags.image} />
+	<!-- Icons -->
+	<meta name="image" content={tags.image} />
+	<link rel="icon" type="image/png" href={tags.image} />
+	<link rel="apple-touch-icon" href={tags.image} />
+</svelte:head>
---
+++ src/routes/index.svelte
@@ -0,0 +1,101 @@
+<script>
+	import Meta from '$lib/components/Meta.svelte';
+</script>
+
+<Meta />
+
+<main>
+	<header>
+		<img class="logo" src="./logo.svg" alt="VerticalVision logo" draggable="false" />
+		<div class="wordmark">
+			<img src="./vertical.svg" alt="Vertical" draggable="false" />
+			<img src="./vision.svg" alt="Vision" draggable="false" />
+		</div>
+	</header>
+	<section>
+		<h1>Site en construction</h1>
+		<p>Le site n'est pas encore prêt, mais vous pouvez quand même nous contacter</p>
+		<div role="group" class="buttons">
+			<a class="button" role="button" href="mailto:v.voisin@protonmail.com">Par mail</a>
+			<a class="button" role="button" href="tel:+33668387662">Par téléphone</a>
+		</div>
+	</section>
+</main>
+
+<style lang="scss">
+	main {
+		padding: 3rem;
+		header {
+			user-select: none;
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+			gap: 2rem;
+			width: 70%;
+			img.logo {
+				width: 16rem;
+			}
+			.wordmark {
+				display: flex;
+				align-items: center;
+				& > * {
+					height: 3rem;
+				}
+			}
+		}
+		gap: 2rem;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		background-color: #f3f9ff;
+
+		.buttons {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+			margin-top: 1rem;
+			gap: 1rem;
+			.button {
+				background-color: #3a7dc9;
+				text-decoration: none;
+				border-radius: 2rem;
+				color: white;
+				padding: 1em;
+				width: 200px;
+				user-select: none;
+				transition: cubic-bezier(0.19, 1, 0.22, 1) 0.3s;
+				cursor: pointer;
+				&:hover {
+					background-color: #1c4371;
+					transition: cubic-bezier(0.19, 1, 0.22, 1) 0.3s;
+				}
+			}
+		}
+	}
+	@media (max-width: 768px) {
+		main {
+			header {
+				width: 100%;
+				img.logo {
+					width: 50%;
+				}
+				.wordmark {
+					align-self: center;
+					flex-direction: column;
+					width: -webkit-fill-available;
+					gap: 1rem;
+				}
+			}
+			.buttons {
+				flex-direction: column;
+				.button {
+					width: 100%;
+				}
+			}
+		}
+	}
+</style>
